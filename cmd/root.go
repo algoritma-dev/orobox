f663a71b0493b2ab153b14dbaea39deb6949073d
Raffaele Carelle <raffaele.carelle@algoritma.it>
2026-03-27 16:58:12 +0100

    bump version to 0.0.9-dev

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ import (
 var cfgFile string
 
 // Version is the current version of the tool.
-var Version = "0.0.8-dev"
+var Version = "0.0.9-dev"
 
 var rootCmd = &cobra.Command{
 	Use:     "oro",
