3a1856c8dc70233e9115fe823ff00124ebff5f66
Raffaele Carelle <raffaele.carelle@algoritma.it>
2026-04-03 09:14:13 +0200

    bump version to 0.0.13-dev and update installation instructions in README

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ import (
 var cfgFile string
 
 // Version is the current version of the tool.
-var Version = "0.0.12-dev"
+var Version = "0.0.13-dev"
 
 var rootCmd = &cobra.Command{
 	Use:     "oro",
