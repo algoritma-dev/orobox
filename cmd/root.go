0a515cd70a3aa3584815e1be726f92b6d729093c
Raffaele Carelle <raffaele.carelle@algoritma.it>
2026-04-17 10:37:14 +0200

    bump version to 1.0.0-rc2 in code and README instructions

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ import (
 var cfgFile string
 
 // Version is the current version of the tool.
-var Version = "1.0.0-rc1"
+var Version = "1.0.0-rc2"
 
 var rootCmd = &cobra.Command{
 	Use:     "orobox",
