8af3c0582cfea7c20800b5da7e83e0f873ad5d55
Raffaele Carelle <raffaele.carelle@algoritma.it>
2026-07-01 15:47:48 +0200

    Bump version to 1.0.0-rc9 in code and update installation instructions in README

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ import (
 var cfgFile string
 
 // Version is the current version of the tool.
-var Version = "1.0.0-rc8"
+var Version = "1.0.0-rc9"
 
 var rootCmd = &cobra.Command{
 	Use:     "orobox",
